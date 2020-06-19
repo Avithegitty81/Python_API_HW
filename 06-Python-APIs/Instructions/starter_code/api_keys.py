3267c177a6201b3917b5566d9543fb7c70343eb4
Avithegitty81 <64101146+Avithegitty81@users.noreply.github.com>
2020-06-19 13:46:46 -0500

    home-work

--- 06-Python-APIs/Instructions/starter_code/api_keys.py
+++ 06-Python-APIs/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "9665f1b93d388ff879554bdbf84c0d33"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBr27DSRFzSSQpZozqckAMj0vyvZN5aSSQ"
